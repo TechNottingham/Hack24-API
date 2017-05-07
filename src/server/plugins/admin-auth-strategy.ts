f4f00de224b15687748827625224810183967463
David Wood <david.p.wood@gmail.com>
2017-05-07 12:25:41 +0100

    Add admin username in callback for admin-auth-strategy

--- src/server/plugins/admin-auth-strategy.ts
+++ src/server/plugins/admin-auth-strategy.ts
@@ -15,7 +15,7 @@ const register: PluginRegister = (server, pluginOptions: CustomOptions, next) =>
       if (username !== pluginOptions.username || password !== pluginOptions.password) {
         return callback(null, false)
       }
-      callback(null, true, {})
+      callback(null, true, { username })
     },
   })
 
