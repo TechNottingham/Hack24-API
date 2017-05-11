cc0c6044b02440993cdfa459752ad42c1a5ddc24
David Wood <david.p.wood@gmail.com>
2017-05-11 08:06:58 +0100

    Fix swapped health-check and root routes, and add test

--- src/server/routes/health-check/index.ts
+++ src/server/routes/health-check/index.ts
@@ -1,37 +1,12 @@
+
 import { PluginRegister } from '../../../hapi.types'
-import { Root } from '../../../resources'
 
 const register: PluginRegister = (server, _, next) => {
 
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (__, reply) => {
-      const rootResponse: Root.TopLevelDocument = {
-        jsonapi: {
-          version: '1.0',
-        },
-        links: {
-          self: '/',
-          teams: {
-            href: '/teams',
-          },
-          users: {
-            href: '/users',
-          },
-          attendees: {
-            href: '/attendees',
-          },
-          hacks: {
-            href: '/hacks',
-          },
-          challenges: {
-            href: '/challenges',
-          },
-        },
-      }
-      reply(rootResponse)
-    },
+    path: '/api',
+    handler: (__, reply) => reply('Hack24 API is running').type('text/plain'),
     config: {
       cors: true,
     },
@@ -41,7 +16,7 @@ const register: PluginRegister = (server, _, next) => {
 }
 
 register.attributes = {
-  name: 'root-routes',
+  name: 'health-check-routes',
   version: '0.0.0',
 }
 
--- src/server/routes/root/index.ts
+++ src/server/routes/root/index.ts
@@ -1,12 +1,36 @@
 import { PluginRegister } from '../../../hapi.types'
+import { Root } from '../../../resources'
 
 const register: PluginRegister = (server, _, next) => {
 
   server.route({
     method: 'GET',
-    path: '/api',
+    path: '/',
     handler: (__, reply) => {
-      reply('Hack24 API is running')
+      const rootResponse: Root.TopLevelDocument = {
+        jsonapi: {
+          version: '1.0',
+        },
+        links: {
+          self: '/',
+          teams: {
+            href: '/teams',
+          },
+          users: {
+            href: '/users',
+          },
+          attendees: {
+            href: '/attendees',
+          },
+          hacks: {
+            href: '/hacks',
+          },
+          challenges: {
+            href: '/challenges',
+          },
+        },
+      }
+      reply(rootResponse)
     },
     config: {
       cors: true,
@@ -17,7 +41,7 @@ const register: PluginRegister = (server, _, next) => {
 }
 
 register.attributes = {
-  name: 'health-check-routes',
+  name: 'root-routes',
   version: '0.0.0',
 }
 
